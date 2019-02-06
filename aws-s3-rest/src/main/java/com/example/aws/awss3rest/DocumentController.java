eafcc7a1a5d1b360d123bad9e413d8cf9ac10fb5
saiyantan.ghosh@gmail.com <saiyantan.ghosh@gmail.com>
2019-02-06 21:00:01 +0530

    file download

--- aws-s3-rest/src/main/java/com/example/aws/awss3rest/DocumentController.java
+++ aws-s3-rest/src/main/java/com/example/aws/awss3rest/DocumentController.java
@@ -1,16 +1,24 @@
 package com.example.aws.awss3rest;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 @RestController
@@ -33,6 +41,19 @@ public class DocumentController {
 			files.add(os.getKey());
 		}
 		return files;
+	}
+	
+	@GetMapping(path="/{documentName}",produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<InputStreamResource> getSingleFile(@PathVariable String documentName) throws IOException {
+		
+		S3Object s3object = this.awsS3Client.getObject(bucketName, documentName);
+		S3ObjectInputStream inputStream = s3object.getObjectContent();
+		
+		return ResponseEntity.ok().contentType(org.springframework.http.MediaType.TEXT_PLAIN).cacheControl(CacheControl.noCache())
+	            .header("Content-Disposition", "attachment; filename=" + documentName)
+	            .body(new InputStreamResource(inputStream));
+		
+		
 	}
 
 }
